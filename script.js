aba280d28609c8d0072d6bb5bd5ca89ea61aca2b
Darya <darya.lapata@gmail.com>
2022-03-02 22:10:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const url = 'https://fa-frontend-code-test.herokuapp.com/getdata'
+const heroBannerSection = document.getElementById("heroBannerId")
+const profilesSection = document.getElementById("profilesId")
+
+// divide code into functions ok! 
+// style with flexbox 
+// responsive
+// publish on github 
+
+const fetchData = (url) => {
+	fetch(url)
+	.then((res) => {
+		return res.json()
+	})
+	.then((data) => {
+		let heroBanner = data.data.heroBanner 
+		createHeroBanner(heroBanner)
+	
+		let profiles = data.data.profiles; 
+		createProfiles(profiles)
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
+}
+
+fetchData(url)
+
+
+const createHeroBanner = (heroBanner) => {
+	heroBannerSection.innerHTML = 
+	`
+		<div class="heroBanner">
+			<article class="article">
+			<h2>${heroBanner.title}</h2>
+			<p>${heroBanner.paragraph}</p>
+		</article>
+		<video controls autoplay loop>
+			<source src="${heroBanner.videoUrl}" type="video/mp4">
+		</video>
+		</div>
+
+	`
+}
+
+const createProfiles = (profiles) => {
+	profiles.forEach((item) => {
+		profilesSection.innerHTML += 
+		`
+			<div class="card">
+			${item.imgUrl ? `<img src=${item.imgUrl} />` : `<h2>helloe</h2>`}
+			<div class="profileInfo">
+				<h3>${item.firstName + " " + item.lastName}</h3>
+				<p>${item.expertise}</p>
+			</div>
+		</div>
+
+		`
+	})
+}
