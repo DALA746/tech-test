4e81c47cbedc3527acfcc3aa057744c12565a520
DALA746 <darya.lapata@gmail.com>
2022-03-05 20:30:55 +0100

    commit

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-const url = 'https://fa-frontend-code-test.herokuapp.com/getdata'
-const heroBannerSection = document.getElementById("heroBannerId")
-const profilesSection = document.getElementById("profilesId")
+const url = "https://fa-frontend-code-test.herokuapp.com/getdata";
+const heroBannerSection = document.getElementById("heroBannerId");
+const profilesSection = document.getElementById("profilesId");
 
-// divide code into functions ok! 
-// style with flexbox 
-// responsive
-// publish on github 
+const notFoundImg = "icons/notFound";
 
 const fetchData = (url) => {
-	fetch(url)
-	.then((res) => {
-		return res.json()
-	})
-	.then((data) => {
-		let heroBanner = data.data.heroBanner 
-		createHeroBanner(heroBanner)
-	
-		let profiles = data.data.profiles; 
-		createProfiles(profiles)
-	})
-	.catch(function(error) {
-		console.log(error);
-	});
-}
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let heroBanner = data.data.heroBanner;
+      createHeroBanner(heroBanner);
 
-fetchData(url)
+      let profiles = data.data.profiles;
+      createProfiles(profiles);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+fetchData(url);
 
 const createHeroBanner = (heroBanner) => {
-	heroBannerSection.innerHTML = 
-	`
+  heroBannerSection.innerHTML = `
 		<div class="grid heroBanner">
 			<article class="article">
 			<h2>${heroBanner.title}</h2>
@@ -39,21 +34,21 @@ const createHeroBanner = (heroBanner) => {
 			<source src="${heroBanner.videoUrl}" type="video/mp4">
 		</video>
 		</div>
-
-	`
-}
+	`;
+};
 
 const createProfiles = (profiles) => {
-	profiles.forEach((item) => {
-		profilesSection.innerHTML += 
-		`
-			<div class="card">
-			<img src=${item.imgUrl} />
+  profiles.forEach((item) => {
+    profilesSection.innerHTML += `
+		<div class="card">
+			<img src=${
+        item.imgUrl
+      } onError="this.onerror=null; this.src='/icons/noImage.jpeg';"/>
 			<div class="profileInfo">
 				<h3>${item.firstName + " " + item.lastName}</h3>
 				<p>${item.expertise}</p>
 			</div>
 		</div>
-		`
-	})
-}
+		`;
+  });
+};
